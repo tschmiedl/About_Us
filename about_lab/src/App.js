69b074a1a5ea8474b2fdf155f3a83a9ad72f187a
tschmiedl <tschmiedl@me.com>
2022-12-15 12:18:40 -0800

    My page is done

--- about_lab/src/App.js
+++ about_lab/src/App.js
@@ -1,14 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
 import { Routes, Route, Link } from "react-router-dom";
+
+import Tanner from './pages/Tanner';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <div className="App">
+      <Nav />
     <Routes>
-      <Route path="/desiree" element={<Desiree />} />
+      {/* <Route path="/desiree" element={<Desiree />} /> */}
       <Route path="/tanner" element={<Tanner />} />
-      <Route path="/milad" element={<Milad />} />
+      {/* <Route path="/milad" element={<Milad />} /> */}
 
 
 
---
+++ about_lab/src/pages/Tanner/index.js
@@ -0,0 +1,11 @@
+const Tanner = () => {
+
+    return(
+        <div>
+        <h1>Tanner Schmiedl</h1>
+        
+        </div>
+    )
+}
+
+export default Tanner
